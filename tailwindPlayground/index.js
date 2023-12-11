ae51782ad63daae6a84eab97f9fea2df5c954116
suryanshGuleria <suryanshguleria05@gmail.com>
2023-12-11 21:41:25 +0530

    Completely added the backend of password generator.
    Here i learned how to generate random numbers, how to use an array of functions, how to shuffle an array.
    The next step is to add styling to this app.

--- tailwindPlayground/index.js
+++ tailwindPlayground/index.js
@@ -2,7 +2,7 @@
 
 const inputSlider = document.querySelector("[data-lengthSlider]");
 const lengthDisplay = document.querySelector("[data-lengthNumber]");
-const passwordDisplay = document.querySelector("[data-passwordDisplay]");
+const passwordDisplay = document.querySelector("[data-passwordDispaly]");
 const copyBtn = document.querySelector("[data-copy]");
 const copyMsg = document.querySelector("[data-copyMsg]");
 const uppercaseCheck = document.querySelector("#uppercase");
@@ -17,13 +17,13 @@ const symbols = '~`!@#$%^&*()_-+={[}]|:;"<,>.?/';
 // setting default values of few variables
 
 let password = "";
-let passwordLength = 17;
-let checkCount = 1;
+let passwordLength = 10;
+let checkCount = 0;
 handleSlider();
 // set strengthCircle color to grey
 
 
-//set passwordLength
+//set passwordLength (used to reflect password length screen in response to slider slide)
 function handleSlider(){
     inputSlider.value = passwordLength;
     lengthDisplay.innerText = passwordLength;
@@ -33,6 +33,10 @@ function setIndicator(color){
     indicator.style.backgroundColor = color;
     //shadow
 }
+
+
+//function to generate random numbers and characters
+
 
 function getRndInteger(min, max){
      return Math.floor(Math.random()*(max-min))+min;
@@ -46,10 +50,16 @@ function generateUpperCase(){
     return String.fromCharCode(getRndInteger(65,91))
 }
 
+function generateLowerCase() {  
+    return String.fromCharCode(getRndInteger(97,123))
+}
+
 function generateSymbol(){
     const randNum = getRndInteger(0, symbols.length);
     return symbols.charAt(randNum);
 }
+
+//Working with CheckBoxes
 
 function calcStrength() {
     let hasUpper = false;
@@ -74,6 +84,29 @@ function calcStrength() {
     }
 }
 
+function handleCheckBoxChange(){
+    checkCount = 0;
+    allCheckBox.forEach((checkbox)=>{
+        if(checkbox.checked){
+            checkCount++;
+        }
+    })
+
+
+    //special condition
+
+    if(passwordLength<checkCount){
+        passwordLength = checkCount;
+        handleSlider();
+    }
+}
+
+allCheckBox.forEach((checkbox)=>{
+     checkbox.addEventListener('change', handleCheckBoxChange);
+})
+
+// To copy the password to the clipboard
+
 async function copyContent(){
     try{
         await navigator.clipboard.writeText(passwordDisplay.value);
@@ -91,5 +124,91 @@ async function copyContent(){
        copyMsg.classList.remove("active");
     },2000);
 }
+
+
+//To shuffle the password
+function shufflePassword(array){
+   //Fisher Yates Method
+   for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+ let str = "";
+ array.forEach((el) => (str += el));
+ return str;
+}
+
+//Adding Event Listners
+
+
+inputSlider.addEventListener('input', (e)=>{
+    passwordLength = e.target.value;
+    handleSlider();
+})
+
+copyBtn.addEventListener('click', ()=>{
+    if(passwordDisplay.value){
+        copyContent();
+    }
+})
+
+
+generateBtn.addEventListener('click', ()=>{
+    // if none of the checkbox are selected
+
+    if(checkCount <= 0) return;
+
+    if(passwordLength < checkCount){
+        passwordLength = checkCount;
+        handleSlider();
+    }
+
+    // to find new password
+
+    // 1st remove old password
+
+    password = "";
+
+    // 2nd Let's put the checked(mandatory) stuff
+
+    let funcArr = [];
+
+    if(uppercaseCheck.checked) funcArr.push(generateUpperCase);
+
+    if(lowercaseCheck.checked) funcArr.push(generateLowerCase);
+
+    if(numbersCheck.checked) funcArr.push(generateRandomNumber);
+
+    if(symbolsCheck.checked) funcArr.push(generateSymbol);
+
+    // compulsoury addition
+
+    for(let i = 0; i<funcArr.length; i++){
+        password += funcArr[i]();
+    }
+
+    // remaining additions
+
+    for(let i = 0; i<passwordLength - funcArr.length; i++){
+        let randIndex = getRndInteger(0, funcArr.length);
+        password += funcArr[randIndex]();
+    }
+
+
+    // shuffle the password
+
+    password = shufflePassword(Array.from(password));
+   
+    // show in ui
+
+    passwordDisplay.value = password;
+
+    // calculate strength
+
+    calcStrength();
+
+})
 
 
