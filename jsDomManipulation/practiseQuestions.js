08b7f92be30bc537e9917caafb46f109dc12ac4b
suryanshGuleria <suryanshguleria05@gmail.com>
2023-12-19 19:02:59 +0530

    Completed weather app and did 10 js questions.
    Added media queries to make the weather app responsive for any screen size.
    Was sick yesterday so was only able to  do 10 js practise questions.

--- jsDomManipulation/practiseQuestions.js
+++ jsDomManipulation/practiseQuestions.js
@@ -45,4 +45,94 @@
 
 // console.log(vowelCount('aei'));
 
-    
+
+
+ // Write a function called 'countLetters' that takes a string parameter and returns 
+// an object that contains a count of each letter in the string.
+
+
+// function calculateChars(theString){
+//     let returnObj = {};
+    // theString.split('').forEach((el, ind)=>{
+    //      if(ind === 0){
+    //         returnObj[el] = 1;
+    //      }
+    //      if(!returnObj.hasOwnProperty(el)){
+    //          returnObj[el] = 1;
+    //      }
+    //      else{
+    //          returnObj[el] += 1;
+    //      }
+    // });
+//     let workingArray = theString.split('');
+//     for(let i = 0 ; i < workingArray.length; i++){
+//         if(i === 0){
+//             returnObj[workingArray[i]] = 1;
+//             continue;
+//          }
+//          if(!returnObj.hasOwnProperty(workingArray[i])){
+//              returnObj[workingArray[i]] = 1;
+//              continue;
+//          }
+//          else{
+//              returnObj[workingArray[i]] += 1;
+//              continue;
+//          }
+//     }
+//     return returnObj;
+// }
+
+// console.log(calculateChars('WebDevelopment'));
+
+
+
+// // 4. **Write a function called 'createCounter' that returns a function. 
+// The returned function should increment a counter variable each time it is called and return the current count.**
+
+
+// function createCounter(){
+//     let count = 0;
+//     function counter(){
+//          count++;
+//          return count;
+//     }
+//     return counter;
+// }
+
+// let callCounter = createCounter();
+
+// console.log(callCounter());
+
+
+// 5. **Write a function called 'sumEvenNumbers' that takes an array of numbers as a parameter and returns the sum of all even numbers in the array.**
+
+// function sumEvenNumbers(arr){
+//     let sum = 0;
+//     for(let value of arr){
+//         if(value%2 === 0){
+//             sum += value;
+//         }
+//     }
+//     return sum;
+// }
+
+// let arr = [1,2,3,4,5,6];
+// console.log(sumEvenNumbers(arr));
+
+
+// 7. **Write a function that takes an array of strings as an argument and returns a new array with only the strings that have a length greater than 5.**
+
+
+// function stringLengthFinder(arr){
+//     let ansArr = [];
+//     for(let value of arr){
+//         if(value.length > 5){
+//            ansArr.push(value);
+//         }
+//     }
+//     return ansArr;
+// }
+
+// let arr = ['hey', 'how', 'are', 'suryansh', 'married', 'ayushi'];
+// console.log(stringLengthFinder(arr));
+
