e2b0f88404940989557f1ae800d86f9189a95971
suryanshGuleria <suryanshguleria05@gmail.com>
2024-01-07 00:11:48 +0530

    Built a testimonial section of a page.
    Using React built a testimonial section of a webapp. The idea was to start working towards a big project so basically we are building small components of that app.

---
+++ REACT/testimonial-slider/src/components/Card.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
+
+export const Card = (props) => {
+  let review = props.review;
+  return (
+    <div className="flex flex-col md:relative ">
+      <div className="absolute top-[-7rem] z-[10] mx-auto">
+        <img
+          className="aspect-square rounded-full w-[140px] h-[140px] z-25"
+          src={review.image}
+          alt=""
+        />
+        <div className="w-[140px] h-[140px] bg-violet-500 rounded-full absolute top-[-6px] z-[-1] left-[10px]"></div>
+      </div>
+
+      <div className="text-center mt-7">
+        <p className="font-bold text-2xl tracking-wide capitalize">
+          {review.name}
+        </p>
+        <p className="text-violet-300 uppercase text-sm">{review.job}</p>
+      </div>
+
+      <div className="text-violet-400 mx-auto mt-5">
+        <FaQuoteLeft></FaQuoteLeft>
+      </div>
+
+      <div className="text-center mt-4 text-slate-500">{review.text}</div>
+
+      <div className="text-violet-400 mx-auto mt-5">
+        <FaQuoteRight></FaQuoteRight>
+      </div>
+    </div>
+  );
+};
---
+++ REACT/testimonial-slider/src/components/Testimonial.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { Card } from "./Card";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+
+function Testimonial(props) {
+  let reviews = props.reviews;
+
+  const [index, setIndex] = useState(4);
+
+  function leftShiftHandler() {
+    if (index - 1 < 0) {
+      setIndex(reviews.length - 1);
+    } else {
+      setIndex(index - 1);
+    }
+  }
+
+  function rightShiftHandler() {
+    if (index + 1 >= reviews.length) {
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
+  }
+
+  function surpriseHandler() {
+    let randomIndex = Math.floor(Math.random() * reviews.length);
+    setIndex(randomIndex);
+  }
+
+  return (
+    <div
+      className="w-[85vw] md:w-[700px] bg-white flex flex-col justify-center 
+    items-center mt-10 p-10 transition-all duration-700 hover:shadow-xl rounded-md"
+    >
+      <Card review={reviews[index]}></Card>
+
+      <div className="flex text-3xl mt-5 gap-3 text-violet-400 font-bold place-content-center">
+        <button
+          onClick={leftShiftHandler}
+          className="cursor-pointer hover:text-violet-500"
+        >
+          <FiChevronLeft></FiChevronLeft>
+        </button>
+        <button
+          onClick={rightShiftHandler}
+          className="cursor-pointer hover:text-violet-500"
+        >
+          <FiChevronRight></FiChevronRight>
+        </button>
+      </div>
+
+      <div className="mt-5">
+        <button
+          onClick={surpriseHandler}
+          className="bg-violet-400 hover:bg-violet-500 transition-all duration-200 cursor-pointer px-10 py-2 rounded-md font-bold text-white"
+        >
+          Surprise me
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonial;
